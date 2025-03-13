f36254fb3db719faecaec1c77246977c7447173d
MSRhie <nanki1004@gmail.com>
2025-03-14 08:38:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.py
+++ 0197-rising-temperature/0197-rising-temperature.py
@@ -1,18 +1,8 @@
 import pandas as pd
 
 def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
-    # step 1. Converting recordDate to Datetime Type
-    weather['recordDate'] = pd.to_datetime(weather['recordDate']) 
-    
-    # Step 2: Sorting the DataFrame
-    weather.sort_values('recordDate', ascending = True, inplace = True)
-
-    # Step 3: Creating Columns for Previous Day's Data
-    weather['before_recordDate'] = weather['recordDate'].shift(1)
-    weather['before_temperature'] = weather['temperature'].shift(1)
-    # Step 4: Filtering for Days with Higher Temperature than the Previous Day
-    result = weather[(weather['recordDate'] == weather['before_recordDate'] + pd.Timedelta(days = 1)) &
-           (weather['temperature'] > weather['before_temperature'])
-    ][['id']].rename({'id':'Id'})
-  
-    return result
+    df = weather.copy()
+    df['id'] = weather['id'] + 1
+    df2 = weather.merge(df, left_on = 'id', right_on = 'id', how = 'left', suffixes  = ['_af', '_bf'])
+    df3 = df2.loc[df2['temperature_af'] - df2['temperature_bf'] > 0, ['id']].rename({'id':'Id'}, axis = 1)
+    return df3
