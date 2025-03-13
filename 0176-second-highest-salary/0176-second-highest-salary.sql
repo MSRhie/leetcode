95d21ebd06452a27d939e678c99aa5dfbd210319
MSRhie <nanki1004@gmail.com>
2025-03-13 14:35:55 +0900

    Time: 468 ms (61.28%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,9 +1,12 @@
 SELECT 
-    CASE 
-        WHEN (SELECT COUNT(DISTINCT salary) FROM Employee) >= 2 
-        THEN (SELECT DISTINCT salary 
-              FROM Employee 
-              ORDER BY salary DESC 
-              LIMIT 1 OFFSET 1)
+    CASE
+        WHEN (SELECT COUNT(DISTINCT salary) FROM Employee) >= 2
+            THEN (
+                SELECT DISTINCT salary
+                FROM Employee
+                ORDER BY salary DESC
+                LIMIT 1
+                OFFSET 1 
+            )
         ELSE NULL
-    END AS SecondHighestSalary;
+    END AS SecondHighestSalary
