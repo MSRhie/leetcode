7de811213479a1fdc1e23d6601a25d4b9f15eeeb
MSRhie <nanki1004@gmail.com>
2025-03-13 14:59:46 +0900

    Time: 2039 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,8 @@
+
 SELECT DISTINCT MAX(salary) AS SecondHighestSalary
-  FROM Employee a
- WHERE Salary< (SELECT MAX(salary) FROM Employee b WHERE b.salary > a.salary)
+FROM Employee
+WHERE salary < (
+    SELECT MAX(salary)
+    FROM Employee
+)
+
