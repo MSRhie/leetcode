091ac7fd631f59c4c722fb520661309cfff8cee0
MSRhie <nanki1004@gmail.com>
2025-03-25 12:06:39 +0900

    Time: 371 ms (83%), Space: 68.7 MB (6.76%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
@@ -1,9 +1,5 @@
 import pandas as pd
 
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    df = employee.merge(employee, left_on = 'managerId', right_on = 'id', suffixes = ('_emp', "_mgr"))
-    result = df[df["salary_emp"] > df["salary_mgr"]][["name_emp"]]
-    result.rename(columns = {'name_emp' : 'Employee'}, inplace = True)
-    return result
-
-    
+    employee = pd.merge(employee,employee, left_on = 'managerId', right_on = 'id', how = 'inner', suffixes = ['_ex','_af'])
+    return employee.loc[employee['salary_af'] < employee['salary_ex'], ['name_ex']].rename({'name_ex':'Employee'}, axis=1)
