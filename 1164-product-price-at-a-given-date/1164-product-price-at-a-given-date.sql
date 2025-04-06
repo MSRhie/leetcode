3ed2e25b0b40088446b2cd8d38bb5543c9a4e621
MSRhie <nanki1004@gmail.com>
2025-04-06 18:43:28 +0900

    Time: 459 ms (93.83%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,8 +1,6 @@
 -- 1. find the prices of all products on 2019-08-16
 -- 2. Assume the price befor any change is 10 (defult)
 WITH temp_data AS (
-    SELECT *
-    FROM (
         SELECT
             product_id,
             new_price,
@@ -10,14 +8,8 @@ WITH temp_data AS (
             ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY product_id, change_date desc) AS 'product_number'
         FROM Products
         WHERE DATE_FORMAT(change_date, '%Y-%m-%d') <= DATE_FORMAT('2019-08-16', '%Y-%m-%d')
-        ) A
-    WHERE product_number = 1
 )
 SELECT DISTINCT A.product_id, 
-    CASE
-        WHEN B.new_price IS NULL 
-            THEN 10
-            ELSE B.new_price
-    END AS price
+    COALESCE(B.new_price, 10) AS price
 FROM Products AS A
-LEFT JOIN temp_data AS B ON A.product_id = B.product_id
+LEFT JOIN temp_data AS B ON A.product_id = B.product_id AND B.product_number = 1
