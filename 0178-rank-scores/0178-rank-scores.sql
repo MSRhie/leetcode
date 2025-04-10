32c1a330dacc72609536190c04e40aa787917e60
MSRhie <nanki1004@gmail.com>
2025-04-10 22:43:42 +0900

    Time: 287 ms (96.73%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,11 @@
+# 1) 점수 랭크는 가장 높은 값에서 낮은 순으로
+# 2) 두 점수간 중복이 발생하면 두 점수는 같은 랭크를 가진다.
+# 3) 중복 이후 다음 랭킹 은 다음의 연속적 정수 값을 가진다. 랭크간 빈 구멍은 없다.
+# score 이름으로 내림차순으로 정렬해라.
+
+
+SELECT
+    Score,
+    DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+FROM Scores
+ORDER BY Score DESC
