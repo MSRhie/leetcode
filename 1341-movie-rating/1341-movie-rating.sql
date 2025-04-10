80ed960176fbd9161b0437b47b738b420700739c
MSRhie <nanki1004@gmail.com>
2025-04-10 15:40:13 +0900

    Time: 1519 ms (70.4%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,44 @@
+# 1. 많은 수의 영화를 평가한  유저의 이름을 찾아라.
+#  1) 동점인 경우 사전적으로 가장작은 유저이름을 리턴해라.
+# 2. 2020년 2월에 가장 높은 평균으로 점수를 가진 영화를 찾아라.
+#  1) 동점인 경우 발생할 경우 사전적으로 가장작은 영화이름을 리턴해라. 
+
+# 1번 풀이
+WITH solution_one AS (
+SELECT B.name AS 'results'
+FROM (
+SELECT
+    user_id,
+    COUNT(rating) AS cnt_rating,
+    MAX(COUNT(rating)) OVER () AS 'max_cnt_rating'
+FROM MovieRating
+GROUP BY user_id
+) A
+INNER JOIN Users B ON A.user_id = B.user_id
+WHERE max_cnt_rating = cnt_rating
+ORDER BY results
+LIMIT 1
+) , solution_two AS
+(
+SELECT B.title AS 'results'
+FROM (
+SELECT
+    movie_id,
+    AVG(rating) AS 'avg_rating',
+    MAX(AVG(rating)) OVER () AS 'max_avg_rating'
+FROM MovieRating
+WHERE created_at LIKE '2020-02%'
+GROUP BY movie_id
+) A
+INNER JOIN Movies AS B ON A.movie_id = B.movie_id
+WHERE avg_rating = max_avg_rating
+ORDER BY results
+LIMIT 1
+)
+SELECT results
+FROM solution_one
+UNION ALL
+SELECT results
+FROM solution_two
+;
+
