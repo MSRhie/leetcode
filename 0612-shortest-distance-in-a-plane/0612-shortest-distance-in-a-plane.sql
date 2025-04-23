dcd062b317fcf4e194523de1ec0e3722fef1ab6c
MSRhie <nanki1004@gmail.com>
2025-04-24 08:52:35 +0900

    Time: 232 ms (84.83%), Space: 0B (100%) - LeetHub

---
+++ 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
@@ -0,0 +1,16 @@
+# Point2D테이블에서 가장 짧은 거리의 두점 사이의 거리를 구하고 2번째 자리에서 반올림해라
+# 이때 중복 점은 없다 -> 거리가 0인건 없다.
+SELECT
+    MIN(shortest) AS shortest
+FROM
+(
+SELECT
+    A.x AS A_X,
+    A.y AS A_Y,
+    B.x AS B_X,
+    B.y AS B_Y,
+    ROUND(SQRT(POWER(B.x-A.x, 2) + POWER(B.y-A.y, 2)),2) AS shortest
+FROM Point2D AS A
+CROSS JOIN POint2D AS B 
+) A
+WHERE shortest <> 0
