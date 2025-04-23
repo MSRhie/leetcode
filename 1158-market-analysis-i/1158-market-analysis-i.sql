c69583263f5b4fc0902634a815d22316c0ca98bf
MSRhie <nanki1004@gmail.com>
2025-04-23 14:33:46 +0900

    Time: 1285 ms (72.29%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -5,31 +5,32 @@
 # 2) Users테이블의 buyer_id와 INNER JOIN 한 buyer_id
 # 3) buyerid 수
 # 이해 O / 직풀 X / 1시간
-WITH key_id AS
-(
-SELECT
-    User_id,
-    join_date
-FROM Users
-)
-SELECT
-    A.user_id AS 'buyer_id',
-    join_date,
-    COUNT(buyer_id) AS 'orders_in_2019'
-FROM key_id AS A
-LEFT JOIN Orders AS B ON A.user_id = B.buyer_id
-AND EXTRACT(year FROM order_date) = '2019'
-GROUP BY buyer_id, join_date
-
-
+# 내가 푼 쿼리
+-- WITH key_id AS
+-- (
 -- SELECT
---   u.user_id AS buyer_id,
---   u.join_date,
---   COUNT(o.order_id) AS orders_in_2019
--- FROM Users AS u
--- LEFT JOIN Orders AS o
---   ON u.user_id = o.buyer_id
---   AND YEAR(o.order_date) = 2019
--- GROUP BY
---   u.user_id,
---   u.join_date;
+--     User_id,
+--     join_date
+-- FROM Users
+-- )
+-- SELECT
+--     A.user_id AS 'buyer_id',
+--     join_date,
+--     COUNT(buyer_id) AS 'orders_in_2019'
+-- FROM key_id AS A
+-- LEFT JOIN Orders AS B ON A.user_id = B.buyer_id
+-- AND EXTRACT(year FROM order_date) = '2019'
+-- GROUP BY buyer_id, join_date
+
+# 개선된 풀이
+SELECT
+  u.user_id AS buyer_id,
+  u.join_date,
+  COUNT(o.order_id) AS orders_in_2019
+FROM Users AS u
+LEFT JOIN Orders AS o
+  ON u.user_id = o.buyer_id
+  AND YEAR(o.order_date) = 2019
+GROUP BY
+  u.user_id,
+  u.join_date;
