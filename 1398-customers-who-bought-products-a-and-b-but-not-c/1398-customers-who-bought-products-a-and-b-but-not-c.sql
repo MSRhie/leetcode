2013a7694bcafbd58df091b0754ffe907ba43139
MSRhie <nanki1004@gmail.com>
2025-04-30 10:54:05 +0900

    Time: 660 ms (73.03%), Space: 0B (100%) - LeetHub

--- 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,5 +1,10 @@
 # A,B를 산 고객들의 이름과 id를 보고 해라. 그러나 C를 사지말아야한다. C를 추천하고 싶기 떄문.
 # customer_id로 오름차순해라
+# 이해 O / 직풀 O /23분
+# 이번 예시처럼 product_id의 A,B를 모두 포함하는, 같은 열에 조건을 두개 걸어야 하는 경우
+# 단순히 WHERE문으로 해결이 불가능하다.
+# 이때 HAVING SUM(product_name) > 0 AND SUM(product_name) > 0 AND SUM(product_name) = 0 을 활용한다.
+
 WITH base AS
 (
 SELECT
