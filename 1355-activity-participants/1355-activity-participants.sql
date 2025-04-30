b1cdb3840fa142998b64667475d486dd1868cc79
MSRhie <nanki1004@gmail.com>
2025-04-30 10:25:21 +0900

    Time: 723 ms (73.07%), Space: 0B (100%) - LeetHub

--- 1355-activity-participants/1355-activity-participants.sql
+++ 1355-activity-participants/1355-activity-participants.sql
@@ -4,8 +4,9 @@
 # 솔루션
 # 1) 최고나 최저가 아닌 참가자수를 가진 활동수를 찾아라.
 # 2) 각 Acticities의 활동테이블은 Friends 테이블의 어떤사람이든 수행한 것이다.
-
-
+# 이해O / 직풀 O / 16분
+# HAVING에서는 중복 집계함수 사용이 안됨.ex. HAVING MAX(COUNT())
+# > COUNT(*) 하고 (SELECT ~ FROM ~ GROUP BY ~ ORDER BY ~ LIMIT 1) 처럼 MAX()함수를 일일이 코딩해야함.
 SELECT
     DISTINCT
     activity
