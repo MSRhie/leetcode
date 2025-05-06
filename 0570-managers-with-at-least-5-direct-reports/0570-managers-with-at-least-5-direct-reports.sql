9923f3640234a51f1a27cfbcc40d379bb073880b
MSRhie <nanki1004@gmail.com>
2025-05-06 09:57:26 +0900

    Time: 336 ms (86.38%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,11 +1,10 @@
 # Write your MySQL query statement below
+# 적어도 5번 직접 보고된 매니저들을 찾아라
+# managerId별 id가 최소 5번 이상 카운트 되는 managerId를 찾아라.
 
-SELECT name
-FROM Employee AS A
-INNER JOIN (
-    SELECT managerId
-    FROM Employee
-    GROUP BY managerId
-    HAVING COUNT(managerId) >= 5
-) B
-ON A.id = B.managerId
+SELECT
+    B.name
+FROM Employee A
+INNER JOIN Employee B ON A.managerId = B.id
+GROUP BY B.id 
+HAVING COUNT(B.id) >= 5
