d9ada417ce537cc9b66dcc7fc141c3df40b9ab1b
MSRhie <nanki1004@gmail.com>
2025-05-08 12:40:45 +0900

    Time: 467 ms (80.97%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,15 +1,17 @@
--- 1. find the prices of all products on 2019-08-16
--- 2. Assume the price befor any change is 10 (defult)
-WITH temp_data AS (
-        SELECT
-            product_id,
-            new_price,
-            change_date,
-            ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY product_id, change_date desc) AS 'product_number'
-        FROM Products
-        WHERE DATE_FORMAT(change_date, '%Y-%m-%d') <= DATE_FORMAT('2019-08-16', '%Y-%m-%d')
+# 2019-08-16 날짜의 모든 제품들의 가격을 찾아라. 단, 모든 제품들의 가격은 변화하기전 가격은 10으로 가정한다.
+# 2019-08-16 이전의 product_id값이 포함안될 수 있으므로, product_id기준과 change_date 2019-08-16을 기준으로 product_id가 유닉한 A LEFT self join A'한다.
+# A'의 product_id별 new_price의 마지막 날짜 값을 구한다.
+# A'의 product_id가 NULL이면 10의 값을 대입한다.
+WITH key_table AS
+(
+SELECT
+    DISTINCT
+    product_id
+FROM Products
 )
-SELECT DISTINCT A.product_id, 
-    COALESCE(B.new_price, 10) AS price
-FROM Products AS A
-LEFT JOIN temp_data AS B ON A.product_id = B.product_id AND B.product_number = 1
+SELECT
+    A.product_id,
+    IFNULL(new_price,10) AS price
+FROM key_table A
+LEFT JOIN Products B ON A.product_id = B.product_id
+AND (A.product_id, B.change_date) IN (SELECT product_id, MAX(change_date) FROM Products WHERE change_date <= '2019-08-16' GROUP BY product_id)
