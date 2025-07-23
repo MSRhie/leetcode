4046fa28faf790354850e150f9b3a7d1a165bb40
MSRhie <nanki1004@gmail.com>
2025-07-23 15:34:03 +0900

    Time: 965 ms (9.44%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,18 +1,25 @@
 # 1. 2019-08-16 기준 이전 날짜 필터 -> 해당 날짜의 product id set A
 # 2. 전체 product들 중 16일 가격 변동 이력이 있는 set A외 product id set B
 # 3. 1와 2외의 product_id가 set C
-
-WITH Set_A AS(
-SELECT
-    DISTINCT
-    product_id,
-    MAX(new_price) OVER (PARTITION BY product_id ORDER BY change_date DESC) AS price
-FROM Products
-WHERE '2019-08-16' >= change_date
+WITH make_rank AS
+(
+    SELECT
+        *,
+        DENSE_RANK() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS rank_id
+    FROM Products
+    WHERE change_date <= '2019-08-16'
 )
 SELECT
     DISTINCT
     IF(B.product_id IS NULL, A.product_id, B.product_id) AS product_id,
-    IF(B.price IS NULL, 10, B.price) AS price
-FROM Products A
-LEFT OUTER JOIN Set_A B ON A.product_id = B.product_id
+    IF(B.new_price IS NULL, 10, B.new_price) AS price 
+FROM Products A 
+LEFT OUTER JOIN (
+                SELECT
+                    *
+                FROM make_rank
+                WHERE rank_id = 1
+                ) B
+ON A.product_id = B.product_id
+# 알게된 것
+# 1. window 함수는 가장 마지막에 돌아감 > RANK에 영향을주는 ORDER BY를 WHERE문으로 줄때 해당 쿼리 내 삽입할 것
