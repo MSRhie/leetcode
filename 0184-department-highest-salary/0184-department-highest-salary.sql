0d2e8983b752b5b2d9cec036cbe06427d988e1db
MSRhie <nanki1004@gmail.com>
2025-08-13 16:15:16 +0900

    Time: 653 ms (66.01%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,14 +1,14 @@
-# 1) 각 부서별로 가장 높은 연봉을 받는 사람을 출력해라. (동일연봉이면 같이 출력)
-# 이해 O / 직풀 O
-WITH temp_employee AS (
+# 각 부서들에서 가장높은 연봉을 받는 직원들을 찾아라
+
 SELECT
-    departmentId,
-    name,
-    salary,
-    DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS 'salary_dense'
-FROM Employee
+    B.name AS Department,
+    A.name AS Employee,
+    salary
+FROM Employee A
+LEFT JOIN Department B
+ON A.departmentId = B.id
+WHERE (departmentId, salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
 )
-SELECT B.name AS 'Department', A.name AS 'Employee', A.salary
-FROM temp_employee AS A
-LEFT JOIN Department AS B ON A.departmentId = B.id
-WHERE salary_dense = 1
