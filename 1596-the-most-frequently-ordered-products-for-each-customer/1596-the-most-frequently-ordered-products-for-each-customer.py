5d848300817583443b29591815e756b0edbf5d51
MSRhie <nanki1004@gmail.com>
2025-08-18 17:09:19 +0900

    Time: 519 ms (60.27%), Space: 69.6 MB (65.75%) - LeetHub

--- 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.py
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.py
@@ -3,15 +3,16 @@ import pandas as pd
 def most_frequently_products(customers: pd.DataFrame, orders: pd.DataFrame, products: pd.DataFrame) -> pd.DataFrame:
     cnt_size = (
         orders
+        # customer_id, product_id 별 카운트 / 이때 reset_index로 size 생성 변수 이름 지정 -> groupby이므로 cusotmer_id, product_id 두개 변수 추가 됨
         .groupby(['customer_id', 'product_id']).size()
         .reset_index(name = 'size')
     )
-
+    # 결과가 series 일때 아래와 같이 변수 추가 가능 -> rank 변수 cnt_size 데이터에 추가
     cnt_size['rank'] = (
         cnt_size
         .groupby(['customer_id'])['size'].rank(method = 'min', ascending=False)
     )
-
+    # 필터 
     customer_cnt_max = (
         cnt_size
         .loc[cnt_size['rank'] == 1]
