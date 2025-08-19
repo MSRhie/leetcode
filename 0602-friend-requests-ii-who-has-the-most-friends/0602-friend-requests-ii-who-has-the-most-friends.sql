7603efd8ad2bef82c2cd64623799ca6feb616b06
MSRhie <nanki1004@gmail.com>
2025-08-19 15:50:41 +0900

    Time: 340 ms (21.57%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,24 +1,24 @@
-# 1) 이 id는 request를 보낸 유저아이디, 요청을 받은 유저아이디를 포함함.
-# (친구요청인것 같음)
-# 2) 요청을 받은 유저의 아이디와 요청을 승낙한 date가 있다.
-# 예시
-# 3) requester_id가 3 기준
-# 3-1 요청한 사람(requester_id)은 1,2
-# 3-2 요청 받은 사람(accepter_id)은 4 > requester_id가 있어야 행있음.
-# > 즉 requester_id에 3을 카운트 -> 1 / accepter_id 3 카운트 -> 2
-# > 총 3사람 
-# 이해O / 직풀 O
-WITH temp_table AS (
-SELECT requester_id AS id, COUNT(requester_id) AS cnt_id
+# 가장 친구가 많은 accepter_id를 구하라
+WITH key_id AS
+(
+SELECT
+    id,
+    COUNT(id) AS cnt_id
+FROM (
+SELECT requester_id AS id
 FROM RequestAccepted
-GROUP BY requester_id
 UNION ALL
-SELECT accepter_id, COUNT(accepter_id)
+SELECT accepter_id AS id
 FROM RequestAccepted
-GROUP BY accepter_id
-)
-SELECT id, SUM(cnt_id) AS num
-FROM temp_table
+) A
 GROUP BY id
-ORDER BY num DESC
-LIMIT 1
+)
+SELECT
+    id,
+    cnt_id AS num
+FROM key_id
+WHERE cnt_id = (
+    SELECT
+        MAX(cnt_id)
+    FROM key_id
+)
