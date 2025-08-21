e42c9bb2ddb855159cc88695b547ee82d8285886
MSRhie <nanki1004@gmail.com>
2025-08-21 09:49:42 +0900

    Time: 314 ms (15.83%), Space: 67.8 MB (7.35%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    result = (
+        employee
+        .drop_duplicates('salary')
+        .assign(rank = lambda d: d['salary'].rank(method = 'min', ascending = False))
+        .query("rank == @N")
+        .loc[:, ['salary']]
+        .rename(columns = {'salary' : 'getNthHighestSalary({})'.format(N)})
+        .pipe(lambda d: d if len(d) >= 1 else pd.DataFrame({'getNthHighestSalary({})'.format(N):[None]}))
+    )
+    return result
