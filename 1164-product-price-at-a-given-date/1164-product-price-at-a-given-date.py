18da0c7ed87f7a6d0b65157fd8943340fc8ec504
MSRhie <nanki1004@gmail.com>
2025-08-22 15:56:27 +0900

    Time: 308 ms (91.74%), Space: 68.6 MB (46.3%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.py
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.py
@@ -3,22 +3,22 @@ import pandas as pd
 def price_at_given_date(products: pd.DataFrame) -> pd.DataFrame:
     # 1) product_id 유니크
     unique_id = products[['product_id']].drop_duplicates()
-    
-    cutoff = pd.Timestamp('2019-08-16')
 
-    query_products = (
+    cut_off = pd.Timestamp('2019-08-16')
+
+    duplicated_date = (
         products
-        .assign(date = lambda d: pd.to_datetime(d['change_date']))
-        .loc[lambda d: d['date'] <= cutoff]
-        .sort_values(['product_id', 'date'], ascending = [True, False])
+        .loc[lambda d: d['change_date'] <= cut_off]
+        .sort_values(['product_id', 'change_date'], ascending=False)
         .drop_duplicates(['product_id'], keep='first')
     )
 
     result = (
         unique_id
-        .merge(query_products, on = 'product_id', how='left')
-        .assign(price = lambda d: d['new_price'].where(d['new_price'].isnull() == False, 10))
+        .merge(duplicated_date, on='product_id', how='left')
+        .assign(price = lambda d: d['new_price'].fillna(10))
         .loc[:, ['product_id', 'price']]
     )
+
 
     return result
