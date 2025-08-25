9ca1972182f8e525312349e957effc6d1e1deed7
MSRhie <nanki1004@gmail.com>
2025-08-25 10:42:11 +0900

    Time: 318 ms (20.45%), Space: 67.6 MB (6.19%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def exchange_seats(seat: pd.DataFrame) -> pd.DataFrame:
+
+    even_result = (
+        seat
+        .query('id % 2 == 0')
+        .assign(id = lambda d: d['id'] - 1)
+    )
+
+    odd_result = (
+        seat
+        .query('id % 2 != 0')
+        .assign(id = lambda d: d['id'] + 1)
+    )
+
+    id_max = odd_result['id'].max()
+
+    result = (
+        pd.concat([even_result, odd_result])
+        .sort_values(['id'])
+        .assign(id = lambda d: np.where(d['id'] == d['id'].max(), len(d), d['id']))
+    )
+
+    return result
