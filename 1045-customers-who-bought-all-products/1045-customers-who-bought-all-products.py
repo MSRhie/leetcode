b085d41cd123050beec92b83d6c623f01cfeae0b
MSRhie <nanki1004@gmail.com>
2025-08-29 11:01:55 +0900

    Time: 282 ms (72.05%), Space: 68.3 MB (21.72%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.py
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.py
@@ -1,17 +1,13 @@
 import pandas as pd
 
 def find_customers(customer: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
-    product = (
-        product
-        .rename(columns={'product_key':'product_key_P'})
-    )
+
     result = (
         customer
         .drop_duplicates()
-        .merge(product, right_on='product_key_P', left_on='product_key', how='left')
         .groupby('customer_id', as_index=False)
         .agg(
-            cnt_product = ('product_key_P', 'count')
+            cnt_product = ('product_key', 'count')
         )
         .loc[lambda d: d['cnt_product'] == len(product), ['customer_id']]
     )
