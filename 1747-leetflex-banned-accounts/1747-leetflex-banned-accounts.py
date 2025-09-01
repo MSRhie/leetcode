a52fd0ca339675e642c207d83181b5dffc7043cb
MSRhie <nanki1004@gmail.com>
2025-09-01 17:40:21 +0900

    Time: 399 ms (34.78%), Space: 69.2 MB (28.26%) - LeetHub

---
+++ 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# 로그인 -> 아웃 사이에 다른 ip address가 2개 이상인 id들을 벤해라
+# -> A의 ip주소의 로그인 이후 B의 로그인이 A의 로그아웃시간보다 작다면, 동시접속
+import pandas as pd
+
+
+def leetflex_banned_accnts(log_info: pd.DataFrame) -> pd.DataFrame:
+    return (
+        log_info.merge(log_info, on="account_id")
+        .query("ip_address_x != ip_address_y and login_x <= login_y <= logout_x")[["account_id"]]
+        .drop_duplicates()
+    )
