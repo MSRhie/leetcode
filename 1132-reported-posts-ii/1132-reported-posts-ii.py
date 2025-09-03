f6bdf67f274057faeb044cfe6159cc9e31ac5594
MSRhie <nanki1004@gmail.com>
2025-09-03 14:43:47 +0900

    Time: 487 ms (8.93%), Space: 69.8 MB (21.43%) - LeetHub

--- 1132-reported-posts-ii/1132-reported-posts-ii.py
+++ 1132-reported-posts-ii/1132-reported-posts-ii.py
@@ -6,23 +6,24 @@ def reported_posts(actions: pd.DataFrame, removals: pd.DataFrame) -> pd.DataFram
     
     df = (
         actions
-        .loc[lambda d: d['extra'] == 'spam', ['extra', 'post_id', 'action_date']]
-        .drop_duplicates()
+        # 1) 스팸 신고만 남기고 필요한 열만
+        .loc[lambda d: (d['action'].eq('report')) & (d['extra'].eq('spam')),
+             ['action_date', 'post_id']]
+        # 2) 같은 날 같은 게시글 중복 신고 제거 → ‘게시글’ 단위로 카운트
+        .drop_duplicates(subset=['action_date', 'post_id'])
+        # 3) 제거 여부 붙이기
         .merge(removals, on='post_id', how='left')
+        # 4) 날짜별 집계 (신고된 게시글 수 / 그중 제거된 게시글 수)
         .groupby('action_date', as_index=False)
         .agg(
-            count_spam = ('extra', 'count'),
-            count_remove_spam = ('remove_date', 'count'),
+            reported=('post_id', 'size'),
+            removed=('remove_date', lambda s: s.notna().sum())
         )
-        .assign(
-            average_daily_percent = lambda d: round((d['count_remove_spam'] / d['count_spam'])*100, 2)
-        )
-        .agg(
-            average_daily_percent = ('average_daily_percent', 'mean')
-        )
-        .assign(
-            average_daily_percent = lambda d: round(d['average_daily_percent'], 2)
-        )
+        # 5) 일별 퍼센트 (반올림 X)
+        .assign(average_daily_percent=lambda d: d['removed'] / d['reported'] * 100)
+        # 6) 일별 퍼센트의 평균만 최종 반올림
+        .agg(average_daily_percent=('average_daily_percent', 'mean'))
+        .round(2)
     )
 
     return df
