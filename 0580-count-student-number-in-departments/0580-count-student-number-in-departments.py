e019132df03af4087ea58bc526bea00db251122d
MSRhie <nanki1004@gmail.com>
2025-09-09 10:25:10 +0900

    Time: 334 ms (76.8%), Space: 68.5 MB (60%) - LeetHub

---
+++ 0580-count-student-number-in-departments/0580-count-student-number-in-departments.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def count_students(student: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    student_count = (
+        student
+        .groupby('dept_id')['student_id']
+        .count()
+        .reset_index(name='student_number')
+    )
+
+    result = (
+        department
+        .merge(student_count, on='dept_id', how='outer')
+        .fillna(0)
+        .drop(['dept_id'], axis=1)
+        .sort_values(['student_number', 'dept_name'], ascending=[False, True])
+    )
+
+    return result
