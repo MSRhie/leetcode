f17b58acbb239efdbf2c31753dd3a552bbe8fbf2
MSRhie <nanki1004@gmail.com>
2025-09-22 15:42:03 +0900

    Time: 313 ms (54.39%), Space: 68.6 MB (7.02%) - LeetHub

--- 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.py
+++ 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.py
@@ -6,12 +6,12 @@ def find_continuous_ranges(logs: pd.DataFrame) -> pd.DataFrame:
         logs['log_id']
         .reset_index(name='log_id')
         .assign(
-            diff = lambda d: d['log_id'] - d['index'],
+            diff = lambda d: d['log_id'] - d['index'], # 연속이 아닌 값 그룹핑위해 diff 변수 생성
         )
         .groupby(['diff'], as_index=True)
         .agg(
-            start_id=("log_id","min"),
-            end_id=("log_id","max")
+            start_id=("log_id","min"), # 그룹핑 중 최솟값이 start
+            end_id=("log_id","max") # 그룹핑 중 최댓값이 end
         )
     )
     
