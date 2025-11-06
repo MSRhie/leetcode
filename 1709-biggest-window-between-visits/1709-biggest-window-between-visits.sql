c0ce53240b5a045db2d15865341901250bfd7b63
MSRhie <nanki1004@gmail.com>
2025-11-06 09:57:49 +0900

    Time: 580 ms (18.7%), Space: 0B (100%) - LeetHub

---
+++ 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+# 오늘날짜 2021-1-1
+# user_id별로 각 방문일자에서 마지막으로 방문일이 가장 긴 window를 찾아라
+# (오늘방문, 다음날 방문이 없담 당일)
+SELECT user_id, MAX(diff) AS biggest_window
+FROM
+(
+	SELECT user_id, 
+	   DATEDIFF(LEAD(visit_date, 1, '2021-01-01') OVER (PARTITION BY user_id ORDER BY visit_date), visit_date) AS diff
+	FROM userVisits
+) a
+GROUP BY user_id
+ORDER BY user_id
