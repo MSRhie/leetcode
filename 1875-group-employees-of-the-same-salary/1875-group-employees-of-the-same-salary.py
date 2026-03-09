4172a9401c90f9913dc26946b4bd85b098850b8f
MSRhie <nanki1004@gmail.com>
2026-03-09 23:20:51 +0900

    Time: 372 ms (9.09%), Space: 68.6 MB (72.73%) - LeetHub

--- 1875-group-employees-of-the-same-salary/1875-group-employees-of-the-same-salary.py
+++ 1875-group-employees-of-the-same-salary/1875-group-employees-of-the-same-salary.py
@@ -4,7 +4,7 @@ import pandas as pd
 #  1) 각 팀은 최소 2명 이상, 한명은 제외됨
 #  2) 팀 id가 오름차순으로 salary가 높음
 #     단, 제외된 사람을 팀 id RANK에 제외할 것
-# team_id별 오름차순, 동점일땐 employee_id의 오름차순으로.
+# team_id별 오름차순, 동점일땐 employee_id의 오름차순으로!
 def employees_of_same_salary(employees: pd.DataFrame) -> pd.DataFrame:
     
     result_df = (
